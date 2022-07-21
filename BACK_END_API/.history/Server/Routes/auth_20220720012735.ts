2908fc3fc07739a5f2bc29ce2fc50dc4cdc35627
NewJJFromHK <kevin518ca@gmail.com>
2022-07-20 21:09:40 -0400

    added Util function to support JWT

---
+++ BACK_END_API/.history/Server/Routes/auth_20220720012735.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+const router = express.Router();
+
+//import the controller module
+import {DisplayRegisterPage, ProcessLogoutPage, DisplayLoginPage, ProcessLoginPage, ProcessRegisterPage} from "../Controllers/auth"
+
+
+/* Process Login page. */
+router.post('/login', ProcessLoginPage);
+
+/* Process Register page. */
+router.post('/register', ProcessRegisterPage);
+
+/* Process Logout page. */
+router.get('/logout', ProcessLogoutPage);
+
+export default router;
+
